675bcce6bd2629faab70b7e1c732846df589a89e
Welton Cardoso <bryan_july@hotmail.com>
2021-08-31 14:26:30 -0300

    Atualizado site

---
+++ Readme.md
@@ -0,0 +1,6 @@
+## Tabela Periodica <br/> <br/>
+
+Esta página foi criada para fins educacionais , ela tem uma tabela periodica interativa e esta em desenvolvimento. <br/> <br/>
+
+
+- [Clique para acessar a pagina](https://welton1986.github.io/TabelaPeriodica)
---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+/*function clicou(){
+    if(document.querySelector(".laurencio").classList.contains('azul')){
+        document.querySelector(".laurencio").classList.remove('azul');
+    } else {
+        document.querySelector(".laurencio").classList.add('azul');
+        
+    }
+}*/
+
+
+
